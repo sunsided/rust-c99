a9f0a96ec0d13586193fc2a69b05a162755ed6ea
Markus Mayer <widemeadows@gmail.com>
2024-06-03 16:57:07 +0200

    Add GitHub Actions

--- build.rs
+++ build.rs
@@ -4,17 +4,18 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=c99.h");
+    println!("cargo:rerun-if-changed=src/c99.h");
+    println!("cargo:rerun-if-changed=build.rs");
 
     let pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
-    env::set_var("CFLAGS", format!("-m{pointer_width}"));
+    env::set_var("CFLAGS", format!("-m{}", pointer_width));
 
     let file = PathBuf::from("src").join("c99.h");
 
     let mut builder = bindgen::Builder::default()
         .header(file.to_string_lossy())
-        .clang_arg(format!("-m{pointer_width}"))
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+        .clang_arg(format!("-m{}", pointer_width))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
 
     if cfg!(not(feature = "std")) {
         builder = builder.use_core();
